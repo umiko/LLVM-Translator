41273401ce15af045f6b032c2d518388bc5eee83
Robert Scholl <robertscholl.private@gmail.com>
2019-07-18 20:30:57 +0200

    moving to laptop for comfort

---
+++ src/llvm/Operations.java
@@ -0,0 +1,9 @@
+package llvm;
+
+import java.util.HashMap;
+
+public class Operation {
+    private static HashMap<String, String> operations= new HashMap<>();
+    private static Operation instance = new Operation();
+
+}
